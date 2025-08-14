9a3267233c8f88a48c7436bd35d4fc6bf5c40919
JulianaStadler <julianastadlerberger@gmail.com>
2025-08-14 10:28:11 -0300

    finished the project

--- base.ps1
+++ base.ps1
@@ -1,7 +1,7 @@
 <#-------------- COMO USAR -----------------
 1 - Mova os arquivos (base.ps1, models.json, usecases.json)
 para a mesma pasta e coloque a url dessa pasta na
-variavel $Pasta
+variavel $PastaPrograma
 2 - Coloque o nome do programa que quer criar em $NomePrograma
 3 - Escreva dentro do arquivo models.json o nome dos models em
 formato json e nao criar o dbcontext pois vai ser criado pelo
@@ -13,17 +13,25 @@ escreva o nome de cada use case, caso queira que essa pasta
 com os arquivos esteja dentro de outra pasta apenas escreva
 dentro do json o nome da pasta com os arquivos dentro
 5 - Execute o programa base.ps1, ao terminar a execucao
-pode tirar a pasta e usar o programa pre escrito normalmente
+pode tirar a pasta e usar o programa pre escrito normalmente. Caso 
+o código nao esteja funcionando execute no powershell como admin:
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+E em seguida selecione A
+6 - Pelo terminal acesse o caminho da pasta e crie a variavel
+de ambiente para acessar o banco de dados
+
+Nota: depois de criado voce pode excluir normalmente este programa
+de execucao
 ------------------------------------------#>
 
 echo "Iniciando criacao"
 
 # Pasta onde foi salvo os 3 arquivos, PRECISA TERMINAR COM \
-$Pasta = "C:\Users\Aluno\Desktop\opaopa\ef-core-project-base-generator"
+$PastaPrograma = "C:\Users\Aluno\Desktop\opaopa\ef-core-project-base-generator"
 $NomePrograma = "PastaCriacao"
 
 # Entra no caminho da pasta
-cd $Pasta
+cd $PastaPrograma
 
 # Salva models e use cases em variaveis
 $models = cat models.json -Raw | ConvertFrom-Json
@@ -32,29 +40,65 @@ $usecases = cat usecases.json -Raw | ConvertFrom-Json
 
 
 echo "criando o programa $NomePrograma"
-# mkdir $NomePrograma | cd
-# dotnet new web
-# dotnet new gitignore
+mkdir $NomePrograma | cd
+
+dotnet new web
+dotnet new gitignore
+dotnet add package Microsoft.EntityFrameworkCore
+dotnet add package Microsoft.EntityFrameworkCore.Tools
+dotnet add package Microsoft.EntityFrameworkCore.Design
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+
+"using System.Data;
+using System.Text.RegularExpressions;
+using ${NomePrograma}.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore.Design;" > Program.cs
+
+
 
 # Criando a pasta models e os Models
-# mkdir Models | cd
-# foreach ($model in $models)
-# {
-# ni "$model.cs"
-# "namespace $NomePrograma.Models;
-# public class $model
-# {
-#     public Guid Id { get; set; }
-# }" > "$model.cs"
-# }
+mkdir Models | cd
+foreach ($model in $models)
+{
+ni "$model.cs"
+"namespace $NomePrograma.Models;
+public class $model
+{
+    public Guid Id { get; set; }
+}" > "$model.cs"
+}
 
-# # Criando o DBContext.cs
-# ni "${NomePrograma}DBContext.cs"
+# Criando o DBContext.cs
+ni "${NomePrograma}DBContext.cs"
 
-# cd ../
+"using Microsoft.EntityFrameworkCore;
+namespace ${NomePrograma}.Models;
+public class ${NomePrograma}DbContext(DbContextOptions options) : DbContext(options)
+{" > "${NomePrograma}DBContext.cs"
+
+foreach ($model in $models)
+{
+"    public DbSet<${model}> ${model}s => Set<${model}>();" >> "${NomePrograma}DBContext.cs"
+}
+
+"
+    protected override void OnModelCreating(ModelBuilder model)
+    {" >> "${NomePrograma}DBContext.cs"
+
+foreach ($model in $models)
+{
+"        model.Entity<${model}>();" >> "${NomePrograma}DBContext.cs"
+}
+
+"   }
+}" >> "${NomePrograma}DBContext.cs"
 
 
-# Funcao que gera as pastas models com os arquivos internos
+cd ..
+
+
 
 
 
@@ -66,6 +110,12 @@ function GeraArquivos {
         [string]$nomeUseCase,
         [string]$namespace
     )
+
+    # Salva os namespaces no program.cs
+    $caminhoAtual = pwd
+    cd "${PastaPrograma}/${NomePrograma}"
+    "using $namespace;" >> Program.cs
+    cd $caminhoAtual
 
 # criando payload
 ni "${nomeUseCase}Payload.cs"
@@ -133,6 +183,7 @@ function GeraPastas {
                 GeraPastas -obj $Case.Value -namespace $namespaceNova
                 cd ..
             }
+
         }
     }
     # somente um item comum, ex ""
@@ -162,8 +213,19 @@ public record Result<T>(
 }" > Result.cs
 
 
+# Criando conexao com o banco de dados
+"
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<${NomePrograma}DbContext>(options => {
+    var sqlConn = Environment.GetEnvironmentVariable(`"SQL_CONNECTION`");
+    options.UseSqlServer(sqlConn);
+});
+
+var app = builder.Build();
+
+app.Run();" >> program.cs
 
 
 
-
-
+# Para sair da pasta criada
+cd ..
